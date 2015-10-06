53cc7c2b0f1331e12ec50998058700d59b15c3cb
KimMingyu <charismaangel2014@gmail.com>
2015-10-06 18:32:54 +0900

    멀티 해상도 적용 : NGUI UIRoot 활용 타이틀화면으로 이동하는 기능 추가 : Pause 버튼 (우측 상단)

---
+++ ProjectFE/Assets/Scripts/MultiResolutionUtil.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiResolutionUtil : MonoBehaviour 
+{
+	public float uiBaseWidth = 320.0f;
+	public float uiBaseHeight = 480.0f;
+
+	// resolution setting
+	void Start () 
+//	void Update ()
+	{
+		UIRoot root = gameObject.GetComponent<UIRoot> ();
+		if (root != null)
+		{
+			root.manualHeight = (int)uiBaseHeight;
+			root.minimumHeight = (int)0.0f;
+			root.maximumHeight = (int)200000.0f;
+		}
+		Camera cam = gameObject.GetComponentInChildren<Camera> ();
+		if (cam != null) 
+		{
+			float perX = uiBaseWidth / Screen.width;
+			float perY = uiBaseHeight / Screen.height;
+			float v = (perX > perY) ? perX : perY;
+			cam.orthographicSize = v;
+		}
+		Destroy (gameObject.GetComponent<MultiResolutionUtil>());
+	}
+}
---
+++ ProjectFE/Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneMgr : MonoBehaviour 
+{
+	public void OnTitleScene ()
+	{
+		Application.LoadLevel ("Title");
+	}
+
+	public void OnGame1Scene ()
+	{
+		Application.LoadLevel ("Game_3Match");
+	}
+
+	public void OnGame2Scene ()
+	{
+		Application.LoadLevel ("Game_ColorMatch");
+	}
+
+	public void OnGame3Scene ()
+	{
+		Application.LoadLevel ("Game_LinkMatch");
+	}
+
+	public void OnGame4Scene ()
+	{
+		Application.LoadLevel ("Game_PuzzleDragon");
+	}
+}
