829705e3ec98eb616bf17a09c77de5f5779ff4f7
KimMingyu <charismaangel2014@gmail.com>
2015-10-25 16:24:46 +0900

    애니메이션 적용 중

---
+++ ProjectFE/Assets/02.Scripts/TestTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTest : MonoBehaviour 
+{
+	public GameObject mObj = null;
+	
+	public void OnClickEvent()
+	{
+		Animator _animatior = (Animator)mObj.GetComponent(typeof(Animator));
+		Debug.Log("mAnimatior.GetLayerName(0) : " + _animatior.GetLayerName(0));
+		AnimatorStateInfo _aniStateInfo = _animatior.GetCurrentAnimatorStateInfo(0);
+		Debug.Log("" + _aniStateInfo.GetHashCode());
+		Debug.Log("" + _aniStateInfo.fullPathHash);
+		Debug.Log("" + _aniStateInfo.tagHash);
+		Debug.Log("" + _aniStateInfo.ToString());
+		Debug.Log("" + _aniStateInfo.IsName("Move"));
+		_animatior.SetInteger("KnockBack", 2);
+	}
+}
